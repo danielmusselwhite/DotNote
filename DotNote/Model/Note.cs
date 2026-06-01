2df83e081319f40fede0da7b70d5ce909e775ae3
Daniel Musselwhite <danielmusselwhite@outlook.com>
2026-06-01 11:49:56 +0100

    feat: add basic Note, Notebook, and User models

---
+++ DotNote/Model/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNote.Model
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public int NotebookId { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string FileLocation { get; set; }
+    }
+}
