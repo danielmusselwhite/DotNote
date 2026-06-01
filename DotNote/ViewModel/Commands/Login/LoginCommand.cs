13142828971bbb678828b784d2761f9b0adaa96b
Daniel Musselwhite <danielmusselwhite@outlook.com>
2026-06-01 13:43:50 +0100

    chore: update LoginCommand namespace

--- DotNote/ViewModel/Commands/Login/LoginCommand.cs
+++ DotNote/ViewModel/Commands/Login/LoginCommand.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
 
-namespace DotNote.ViewModel.Commands
+namespace DotNote.ViewModel.Commands.Login
 {
     public class LoginCommand : ICommand
     {
