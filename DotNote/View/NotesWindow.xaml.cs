7ee5444ac5e38bed3be0f36a9687f86f9d4b6906
Daniel Musselwhite <danielmusselwhite@outlook.com>
2026-06-01 13:50:22 +0100

    feat: add menu skeleton, with exit option

--- DotNote/View/NotesWindow.xaml.cs
+++ DotNote/View/NotesWindow.xaml.cs
@@ -21,5 +21,10 @@ namespace DotNote.View
         {
             InitializeComponent();
         }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
