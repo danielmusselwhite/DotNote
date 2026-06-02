4fdb508f5860ab27c94ba1df8081af9ca858e232
Daniel Musselwhite <danielmusselwhite@outlook.com>
2026-06-02 09:19:00 +0100

    feat: add bold button to the toolbar, and a status bar showing character count

--- DotNote/View/NotesWindow.xaml.cs
+++ DotNote/View/NotesWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DotNote.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -27,9 +28,26 @@ namespace DotNote.View
             Application.Current.Shutdown();
         }
 
-        private void SpeechButton_Click(object sender, RoutedEventArgs e)
+        private void speechButton_Click(object sender, RoutedEventArgs e)
         {
+            // todo - placeholder
+        }
 
+        private void rtbContent_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string text = new TextRange(rtbContent.Document.ContentStart, rtbContent.Document.ContentEnd).Text;
+
+            // rich textbox includes a newline character at the end, so we trim it before counting characters
+            int charCount = string.IsNullOrWhiteSpace(text)
+                ? 0
+                : text.TrimEnd('\r', '\n').Length;
+
+            txtStatusMessage.Text = $"Character Count: {charCount}";
+        }
+
+        private void boldButton_Click(object sender, RoutedEventArgs e)
+        {
+            rtbContent.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Bold);
         }
     }
 }
