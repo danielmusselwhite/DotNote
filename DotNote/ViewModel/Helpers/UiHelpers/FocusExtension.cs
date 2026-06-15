1da7490eaa098df58f2280417db7ead4835e9b82
Daniel Musselwhite <danielmusselwhite@outlook.com>
2026-06-15 11:56:30 +0100

    feat: add auto focus to the rename textbox

---
+++ DotNote/ViewModel/Helpers/UiHelpers/FocusExtension.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace DotNote.ViewModel.Helpers.UiHelpers
+{
+    public static class FocusExtension
+    {
+        public static readonly DependencyProperty IsFocusedProperty =
+            DependencyProperty.RegisterAttached(
+                "IsFocused",
+                typeof(bool),
+                typeof(FocusExtension),
+                new PropertyMetadata(false, OnIsFocusedChanged));
+
+        public static bool GetIsFocused(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsFocusedProperty);
+        }
+
+        public static void SetIsFocused(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsFocusedProperty, value);
+        }
+
+        private static void OnIsFocusedChanged(
+            DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
+        {
+            if (d is not UIElement element)
+                return;
+
+            if ((bool)e.NewValue)
+            {
+                element.Dispatcher.BeginInvoke(() =>
+                {
+                    if (element is TextBox tb)
+                    {
+                        tb.Focus();
+                        tb.SelectAll();
+                        Keyboard.Focus(tb);
+                    }
+                }, DispatcherPriority.Loaded);
+            }
+        }
+    }
+}
